22126f8128a5ab6366f7351832954a3c9a68ebf3
maguette <seckmaguettte036@gmail.com>
2023-01-26 14:33:25 +0000

    Initial Commit

---
+++ BaJProjetson/BaJProjetsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaJProjetsonApp.swift
+//  BaJProjetson
+//
+//  Created by Maguette SECK on 26/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct BaJProjetsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

1f8835b4d8835b6d92c3f90fd70ebde19dd0215f
maguette <seckmaguettte036@gmail.com>
2023-02-06 10:09:01 +0000

    environement

---
+++  DetailsView.swift
@@ -0,0 +1,20 @@
+//
+//   DetailsView.swift
+//  BaJProjetson
+//
+//  Created by Maguette SECK on 02/02/2023.
+//
+
+import SwiftUI
+
+struct _DetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct _DetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        _DetailsView()
+    }
+}
